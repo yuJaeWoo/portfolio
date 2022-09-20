42648fe7a155097331948460fd382efb68346fec
yujaewoo <yoo8960@gmail.com>
2022-09-21 00:53:36 +0900

    rework_1

--- portfolio/src/components/body/index.tsx
+++ portfolio/src/components/body/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Body({children}:{children: React.ReactNode}) {
     return (
-        <div className="">
+        <div className="w-full p-0 mr-5">
             {children}
         </div>
     )
--- portfolio/src/components/terminal/index.tsx
+++ portfolio/src/components/terminal/index.tsx
@@ -7,19 +7,19 @@ const Terminal = () => {
         (<div key={0}><p>$ hi there!!</p><br/></div>),
         (<div key={1}><p>$ my name is jaewu Yu</p><br/></div>),
         (<div key={2}><p>$ welcome to my portfolio</p><br/></div>),
-        (<div key={3}><p onClick={()=>{keyDownHandler()}}>&gt; if you want to see it, press Click Me</p><br/></div>)
+        (<div key={3}><p className="goD" onClick={()=>{keyDownHandler()}}>&gt; if you want to see it, press Click Me</p><br/></div>)
     ]
     const [va, setVa] = useState<any>([arr[0]])
     const keyDownHandler = () => {
         alert("go dashboard")
       };
     return (
-        <>
+        <div >
                <div className="typewriter" onClick={()=>{
                 setVa([...va, arr[count]])
                 count++
                 }}>{va}</div>
-        </>
+        </div>
     )
 }
 
