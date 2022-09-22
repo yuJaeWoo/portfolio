0f441d12bcbb13837ae1c560fcc8bacdef141663
yujaewoo <yoo8960@gmail.com>
2022-09-22 21:46:31 +0900

    generator

--- portfolio/src/pages/profile.tsx
+++ portfolio/src/pages/profile.tsx
@@ -1,4 +1,10 @@
 const Profile = () => {
+                //여기서 대화방식의 generator 생성을 해보자.
+    function* chatProfile() {
+        yield "안녕하세요! 반갑습니다."
+        yield "개발자로 성장중인 유재우입니다."
+    }
+
     return (
         <div className="main">
             <div className="typewriter">
