e4a774d43ad5cf0802c9f673b22ab6887301439d
yujaewoo <>
2022-09-23 12:25:45 +0900

    removeResponsive

--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -1,26 +1,26 @@
-import '../../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '@components/layout'
-import useDeviceCheck from '@hooks/useDeviceCheck'
+import "../../styles/globals.css"
+import type { AppProps } from "next/app"
+import Layout from "@components/layout"
 
 function MyApp({ Component, pageProps }: AppProps) {
-const {isDesktopOrMobile} = useDeviceCheck();
-// console.log("isPc?", isDesktopOrMobile)
+    // const {isDesktopOrMobile} = useDeviceCheck();
+    // console.log("isPc?", isDesktopOrMobile)
 
-  return isDesktopOrMobile ? (
-    <div>
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    </div>
-  ) : 
-  (
-    <div>
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    </div>
-  )
+    // return isDesktopOrMobile ? (
+    return (
+        <div>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </div>
+        // ) :
+        // (
+        // <div>
+        //     <Layout>
+        //         <Component {...pageProps} />
+        //     </Layout>
+        // </div>
+    )
 }
 
 export default MyApp
