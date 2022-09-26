049e0095843339e0ea068b4c592a70e4d45d87e6
yujaewoo <>
2022-09-26 09:58:53 +0900

    responsive_3

---
+++ portfolio/src/components/layout/desktop.layout.tsx
@@ -0,0 +1,22 @@
+// src/layouts/desktop_layout.js
+import Body from "@components/body"
+import Footer from "@components/footer"
+import Header from "@components/header"
+import React from "react"
+
+const DesktopLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <div className="flex flex-col justify-between">
+                <Header />
+                <Body>
+                    <div>{children}</div>
+                </Body>
+                <div></div>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default DesktopLayout
--- portfolio/src/components/layout/index.tsx
+++ portfolio/src/components/layout/index.tsx
@@ -7,22 +7,45 @@ import React from "react"
 import Footer from "@components/footer"
 import Desc from "@components/section/desc"
 import FooterDesc from "@components/section/footerDesc"
+import useMediaQuery from "react-responsive/types/useMediaQuery"
+import DesktopLayout from "./desktop.layout"
+import MobileLayout from "./mobile.layout"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const Router = useRouter()
-    // console.log(Router);
+    // const Router = useRouter()
+    // // console.log(Router);
+
+    // return (
+    //     <>
+    //         <div  className="flex flex-col justify-between">
+    //             <Header />
+    //             <Body>
+    //                 <div>{children}</div>
+    //             </Body>
+    //             <div></div>
+    //         </div>
+    //             <Footer />
+    //             </>
+
+    // )
+    const Desktop = () => {
+        const isDesktop = useMediaQuery({ minWidth: 768 })
+        if (isDesktop) {
+            return <DesktopLayout>{children}</DesktopLayout>
+        }
+    }
+
+    const Mobile = () => {
+        const isMobile = useMediaQuery({ maxWidth: 767 })
+        if (isMobile) {
+            return <MobileLayout>{children}</MobileLayout>
+        }
+    }
 
     return (
-        <>
-            <div  className="flex flex-col justify-between">
-                <Header />
-                <Body>
-                    <div>{children}</div>
-                </Body>
-                <div></div>
-            </div>
-                <Footer />
-                </>
-
+        <div>
+            {Desktop && Desktop()}
+            {Mobile && Mobile()}
+        </div>
     )
 }
